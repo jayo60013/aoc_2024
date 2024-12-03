37d84e9aeab2ba9754b9b7db684a9a0883bba52a
John Gatward <john.gatward@fdmgroup.com>
2024-12-03 17:45:46 +0000

    remove target/ on day01

--- day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/home/jay/docs/projects/aoc/2024/day01/target/debug/day01: /home/jay/docs/projects/aoc/2024/day01/src/main.rs
--- day01/target/debug/deps/day01-b06c4bc53051f8ef.d
+++
@@ -1,5 +0,0 @@
-/home/jay/docs/projects/aoc/2024/day01/target/debug/deps/day01-b06c4bc53051f8ef: src/main.rs
-
-/home/jay/docs/projects/aoc/2024/day01/target/debug/deps/day01-b06c4bc53051f8ef.d: src/main.rs
-
-src/main.rs:
