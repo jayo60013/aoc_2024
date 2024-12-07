afa5db1730312fd6abeeb618cb5e5fa8f169d67f
john.gatward <john.gatward@sainsburys.co.uk>
2024-12-07 11:13:44 +0000

    losing my mind on day 6 still

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,5 +1,11 @@
 use std::{collections::HashSet, env, fs};
 
+#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -12,37 +18,16 @@ fn main() {
     let filename = args.get(1).unwrap();
     let contents = fs::read_to_string(filename).unwrap();
 
-    println!("Part 1: {}", part1(&contents));
-    println!("Part 2: {}", part2(&contents));
+    let visited = get_visited(&contents);
+
+    println!("Part 1: {}", visited.len());
+    println!("Part 2: {}", part2(&contents, visited));
 }
 
-fn part1(input: &str) -> usize {
+fn part2(input: &str, visited: HashSet<Point>) -> usize {
     let obstacles = parse_obstacles(input);
-    let (width, height) = get_grid_dimensions(input);
-
-    let mut pos = get_start_pos(input);
-    let mut dir = UP;
-    let mut visited: HashSet<Point> = HashSet::new();
-
-    while check_if_point_in_grid(&pos, width, height) {
-        visited.insert(pos);
-        pos = step_guard(&obstacles, pos, &mut dir, width, height);
-    }
-    visited.len()
-}
-
-fn part2(input: &str) -> usize {
-    let obstacles = parse_obstacles(input);
-    let (width, height) = get_grid_dimensions(input);
     let start_pos = get_start_pos(input);
-    let mut pos = start_pos;
-    let mut dir = UP;
-    let mut visited: HashSet<Point> = HashSet::new();
-
-    while check_if_point_in_grid(&pos, width, height) {
-        visited.insert(pos);
-        pos = step_guard(&obstacles, pos, &mut dir, width, height);
-    }
+    let (width, height) = get_grid_dimensions(input);
 
     visited
         .into_iter()
@@ -65,19 +50,6 @@ fn check_for_cycle(start_pos: Point, obstacles: HashSet<Point>, width: i32, heig
         if !route.insert((pos.x, pos.y, dir.x, dir.y)) {
             return true;
         }
-        pos = step_guard(&obstacles, pos, &mut dir, width, height);
-    }
-    return false;
-}
-
-fn step_guard(
-    obstacles: &HashSet<Point>,
-    pos: Point,
-    dir: &mut Point,
-    width: i32,
-    height: i32,
-) -> Point {
-    loop {
         let np = Point {
             x: pos.x + dir.x,
             y: pos.y + dir.y,
@@ -85,11 +57,38 @@ fn step_guard(
 
         // Guard exited grid or step ahead clear
         if !check_if_point_in_grid(&np, width, height) || !obstacles.contains(&np) {
-            return np;
+            pos = np;
+        } else {
+            dir = rotate_dir(dir);
         }
-
-        *dir = rotate_dir(*dir);
     }
+    return false;
+}
+
+fn get_visited(input: &str) -> HashSet<Point> {
+    let obstacles = parse_obstacles(input);
+    let (width, height) = get_grid_dimensions(input);
+
+    let mut pos = get_start_pos(input);
+    let mut dir = UP;
+    let mut visited: HashSet<Point> = HashSet::new();
+
+    while check_if_point_in_grid(&pos, width, height) {
+        visited.insert(pos);
+
+        let np = Point {
+            x: pos.x + dir.x,
+            y: pos.y + dir.y,
+        };
+
+        // Guard exited grid or step ahead clear
+        if !check_if_point_in_grid(&np, width, height) || !obstacles.contains(&np) {
+            pos = np;
+        } else {
+            dir = rotate_dir(dir);
+        }
+    }
+    visited
 }
 
 const UP: Point = Point { x: 0, y: -1 };
@@ -115,12 +114,6 @@ fn get_grid_dimensions(input: &str) -> (i32, i32) {
     let width = input.split_once('\n').unwrap().0.len() as i32;
     let height = input.lines().count() as i32;
     (width, height)
-}
-
-#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
 }
 
 fn parse_obstacles(input: &str) -> HashSet<Point> {
