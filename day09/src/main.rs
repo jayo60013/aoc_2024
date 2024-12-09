ee5fbaf46f442a42581083c8b35ff6bbe7a789a4
John Gatward <john.gatward@fdmgroup.com>
2024-12-09 23:48:04 +0000

    day 9 clean up

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,6 +1,11 @@
 use std::{env, fs};
 
-use itertools::Itertools;
+#[derive(Debug)]
+struct File {
+    id: u32,
+    start: usize,
+    end: usize,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,28 +20,14 @@ fn main() {
     let contents = fs::read_to_string(filename).unwrap();
 
     let disk_map = parse_input(&contents);
+    let disk = parse_disk(&contents);
 
-    println!("Part 1: {part1_ans}", part1_ans = part1(&disk_map));
-    println!("Part 2: {part2_ans}", part2_ans = part2(&disk_map));
+    println!("Part 1: {part1_ans}", part1_ans = part1(&disk));
+    println!("Part 2: {part2_ans}", part2_ans = part2(&disk_map, &disk));
 }
 
-fn part1(disk_map: &Vec<File>) -> i64 {
-    let mut compressed_disk: Vec<Option<i32>> = Vec::new();
-    let length = disk_map.last().unwrap().end;
-    let mut disk_map_idx: usize = 0;
-
-    for ptr in 0..=length {
-        let file = disk_map.get(disk_map_idx).unwrap();
-
-        if file.start <= ptr && ptr <= file.end {
-            compressed_disk.push(Some(file.id));
-        } else {
-            compressed_disk.push(None);
-        }
-        if ptr >= file.end {
-            disk_map_idx += 1;
-        }
-    }
+fn part1(disk: &Vec<Option<u32>>) -> u64 {
+    let mut compressed_disk = disk.clone();
 
     while compressed_disk.contains(&None) {
         let last = compressed_disk.pop().unwrap();
@@ -52,64 +43,43 @@ fn part1(disk_map: &Vec<File>) -> i64 {
     compressed_disk
         .iter()
         .enumerate()
-        .map(|(i, file_id)| i as i64 * file_id.unwrap() as i64)
+        .map(|(i, file_id)| i as u64 * file_id.unwrap() as u64)
         .sum()
 }
 
-fn part2(disk_map: &Vec<File>) -> i64 {
-    let mut disk: Vec<Option<i32>> = Vec::new();
-    let length = disk_map.last().unwrap().end;
-    let mut disk_map_idx: usize = 0;
-
-    for ptr in 0..=length {
-        let file = disk_map.get(disk_map_idx).unwrap();
-
-        if file.start <= ptr && ptr <= file.end {
-            disk.push(Some(file.id));
-        } else {
-            disk.push(None);
-        }
-        if ptr >= file.end {
-            disk_map_idx += 1;
-        }
-    }
+fn part2(disk_map: &Vec<File>, disk: &Vec<Option<u32>>) -> u64 {
+    let mut compressed_disk = disk.clone();
 
     for file in disk_map.iter().rev() {
         let right_ptr = file.end;
         let window_size = file.end - file.start + 1;
 
-        if let Some(start_idx) = disk
+        if let Some(start_idx) = compressed_disk
             .windows(window_size)
             .position(|window| window == vec![None; window_size])
         {
             if start_idx < right_ptr {
                 for i in start_idx..(start_idx + window_size) {
-                    disk[i] = Some(file.id);
+                    compressed_disk[i] = Some(file.id);
                 }
                 for i in file.start..=file.end {
-                    disk[i] = None;
+                    compressed_disk[i] = None;
                 }
             }
         }
     }
 
-    disk.iter()
+    compressed_disk
+        .iter()
         .enumerate()
         .filter(|(_, x)| x.is_some())
-        .map(|(i, file_id)| i as i64 * file_id.unwrap() as i64)
+        .map(|(i, file_id)| i as u64 * file_id.unwrap() as u64)
         .sum()
-}
-
-#[derive(Debug)]
-struct File {
-    id: i32,
-    start: usize,
-    end: usize,
 }
 
 fn parse_input(input: &str) -> Vec<File> {
     let mut is_filename = true;
-    let mut filename: i32 = 0;
+    let mut filename: u32 = 0;
     let mut ptr: usize = 0;
     let mut files: Vec<File> = Vec::new();
 
@@ -131,4 +101,22 @@ fn parse_input(input: &str) -> Vec<File> {
         }
     }
     files
+}
+
+fn parse_disk(input: &str) -> Vec<Option<u32>> {
+    let mut is_filename = true;
+    let mut filename: u32 = 0;
+    let mut disk: Vec<Option<u32>> = Vec::new();
+
+    for c in input.trim().chars() {
+        let length = c.to_digit(10).unwrap() as usize;
+        let value = if is_filename { Some(filename) } else { None };
+        let mut blocks = vec![value; length];
+        disk.append(&mut blocks);
+        if is_filename {
+            filename += 1;
+        }
+        is_filename = !is_filename;
+    }
+    disk
 }
