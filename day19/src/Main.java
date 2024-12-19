a529b60c2dd6b2bed49febba8bec59d35e04e62a
john.gatward <john.gatward@sainsburys.co.uk>
2024-12-19 15:30:14 +0000

    we're so back

--- day19/src/Main.java
+++ day19/src/Main.java
@@ -7,79 +7,75 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-  public record Input(List<String> patterns, List<String> designs) {
-  }
-
-  public static void main(final String[] args) throws IOException {
-    if (args.length != 1) {
-      System.err.println("Usage: java Day19 <filename.txt>");
-      return;
+    public record Input(List<String> patterns, List<String> designs) {
     }
 
-    final String fileName = args[0];
-    final String contents = Files.readString(Path.of(fileName));
-    final Input input = parseInput(contents);
+    public static void main(final String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: java Day19 <filename.txt>");
+            return;
+        }
 
-    final long start = System.currentTimeMillis();
-    final long part1Ans = part1(input);
-    final long part2Ans = part2(input);
-    final long duration = System.currentTimeMillis() - start;
+        final String fileName = args[0];
+        final String contents = Files.readString(Path.of(fileName));
+        final Input input = parseInput(contents);
 
-    System.out.printf("Part 1: %d\n", part1Ans);
-    System.out.printf("Part 2: %s\n", part2Ans);
-    System.out.printf("Took: %dms\n", duration);
-  }
+        final long start = System.currentTimeMillis();
+        final long part1Ans = part1(input);
+        final long part2Ans = part2(input);
+        final long duration = System.currentTimeMillis() - start;
 
-  public static long part1(final Input input) {
-    return input.designs
-        .stream()
-        .filter(design -> isDesignable(input.patterns, design))
-        .count();
-  }
-
-  public static long part2(final Input input) {
-    return input.designs
-        .stream()
-        .mapToLong(design -> numberOfWays(new HashMap<>(), input.patterns, design))
-        .sum();
-  }
-
-  public static boolean isDesignable(final List<String> patterns, final String design) {
-    if (design.isEmpty())
-      return true;
-
-    for (final String pattern : patterns) {
-      if (design.startsWith(pattern)) {
-        final boolean isPossible = isDesignable(patterns, design.substring(pattern.length()));
-        if (isPossible)
-          return true;
-      }
+        System.out.printf("Part 1: %d\n", part1Ans);
+        System.out.printf("Part 2: %s\n", part2Ans);
+        System.out.printf("Took: %dms\n", duration);
     }
 
-    return false;
-  }
-
-  public static long numberOfWays(final Map<String, Long> lookup, final List<String> patterns, final String design) {
-    if (design.isEmpty())
-      return 1;
-    if (lookup.containsKey(design))
-      return lookup.get(design);
-
-    long ways = 0;
-    for (final String pattern : patterns) {
-      if (design.startsWith(pattern)) {
-        ways += numberOfWays(lookup, patterns, design.substring(pattern.length()));
-      }
+    public static long part1(final Input input) {
+        return input.designs
+                .stream()
+                .filter(design -> isDesignable(input.patterns, design))
+                .count();
     }
 
-    lookup.put(design, ways);
-    return ways;
-  }
+    public static long part2(final Input input) {
+        return input.designs
+                .stream()
+                .mapToLong(design -> numberOfWays(new HashMap<>(), input.patterns, design))
+                .sum();
+    }
 
-  public static Input parseInput(final String input) {
-    final String[] parts = input.split("\n\n");
-    final List<String> patterns = Arrays.stream(parts[0].split(", ")).toList();
-    final List<String> designs = parts[1].lines().toList();
-    return new Input(patterns, designs);
-  }
+    public static boolean isDesignable(final List<String> patterns, final String design) {
+        if (design.isEmpty()) return true;
+
+        for (final String pattern : patterns) {
+            if (design.startsWith(pattern)) {
+                final boolean isPossible = isDesignable(patterns, design.substring(pattern.length()));
+                if (isPossible) return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static long numberOfWays(final Map<String, Long> lookup, final List<String> patterns, final String design) {
+        if (design.isEmpty()) return 1;
+        if (lookup.containsKey(design)) return lookup.get(design);
+
+        long ways = 0;
+        for (final String pattern : patterns) {
+            if (design.startsWith(pattern)) {
+                ways += numberOfWays(lookup, patterns, design.substring(pattern.length()));
+            }
+        }
+
+        lookup.put(design, ways);
+        return ways;
+    }
+
+    public static Input parseInput(final String input) {
+        final String[] parts = input.split("\n\n");
+        final List<String> patterns = Arrays.stream(parts[0].split(", ")).toList();
+        final List<String> designs = parts[1].lines().toList();
+        return new Input(patterns, designs);
+    }
 }
